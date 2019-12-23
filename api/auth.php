d407d9798d5c9aab56b4fc77d923d0d0821654d3
Svitlana <tarasiuksvitlana4@gmail.com>
2019-12-24 00:42:53 +0200

    complete-code cleaning

--- api/auth.php
+++ api/auth.php
@@ -1,41 +1,5 @@
 <?php
 
-// header("Content-Type: application/json; charset=UTF-8");
-// header("Access-Control-Allow-Methods: POST");
-// header("Access-Control-Max-Age: 3600");
-// header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-
-// include_once 'config/config.php';
-// $res = [];
-// if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-//     // $data = json_decode(file_get_contents("php://input"));
-//     // print_r($data);
-
-//     $sql = "SELECT * from api_users where username = '".$_POST['username']."' AND password='".$_POST['password']."'";
-//     // echo $sql;
-//     $result = $conn->query($sql);
-//     // print_r($result);
-    
-//     if($result->num_rows > 0){
-//         $row = $result->fetch_assoc();
-//         $_SESSION['user_id'] = $row['id'];
-//         $res = ['status'=>200, 'message'=>'authenticated successfully.','data'=>['id'=>$row['id'],'username'=>$row['username']]];
-//     }else{
-//         $res = ['status'=>401, 'message'=>'authentication failed.'];
-//     }
-//     echo json_encode($res);
-//     die();
-// }elseif ($_SERVER['REQUEST_METHOD'] === 'DELETE') {
-//     setcookie("user_id", "", time() -1*24*60*60*1000,'/');
-//     $res=['status'=>200,'message'=>'Successfully logout.'];
-//     echo json_encode($res);
-//     die();
-// }else{
-//     $res = ['status'=>400, 'message'=>'Bad request.'];
-//     echo json_encode($res);
-//     die();
-// }
-
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: All");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
--- api/users.php
+++ api/users.php
@@ -1,20 +1,6 @@
 <?php 
 header("Content-Type: application/json; charset=UTF-8");
-// include_once 'config/config.php';
-//     $res = [];
-//     $sql = "SELECT * from students";
-//     $result = $conn->query($sql);
-//     if($result->num_rows >0){
-//         $res = ['status'=>200,'data'=>[]];
-//         while($row = $result->fetch_assoc()) {
-//             $res['data'][]=$row;
-//         }
-//     }else{
-//         $res = ['status'=>404,'data'=>[]];
-//     }
 
-// echo json_encode($res);
-// die();
 require_once 'basic.php';
 require_once 'database.php';
 
